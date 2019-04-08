2233dda260d656da61a07b9d1392548bdb7d6dee
Maren Beam <maren@chro.bid>
2019-04-08 14:23:46 -0400

    It works! This generates 5 arrays full of random integers, and it does so concurrently (each array is generated in its own routine). Next is to allow for the concurrent generation of an arbitrary number of arrays in arbitrarily many routines. I hope this is possible. I could imagine that forking arbitrary numbers of routines may be like, disallowed by the compiler or runtime or something.

--- channels.go
+++ channels.go
@@ -1,21 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+/*
 func sum(nums []int, c chan int) {
-	sum := 0
+	var sum int
 	for _, value := range nums {
 		sum += value
 	}
 	c <- sum
 }
+*/
 
-func gen() int {
-	return
-}
+func gen(c chan []int) {
+	const listLength = 100
+	var num []int
+	rand.Seed(time.Now().UTC().UnixNano())
 
-func main() {
-	length := 100
-	var s []int
-	for i, _ := range s {
-		s[i] = gen()
+	for i := 0; i < listLength; i++ {
+		num = append(num, rand.Intn(999999)) // generate random number here
 	}
+	c <- num
 }
+
+// concurrently generate lists of random numbers
+func main() {
+	listQuant := 5
+	c := make(chan []int)
+
+	for i := 0; i < listQuant; i++ {
+		go gen(c)
+	}
+	num1, num2, num3, num4, num5 := <-c, <-c, <-c, <-c, <-c
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num3)
+	fmt.Println(num4)
+	fmt.Println(num5)
+}
+
+// do we send the entire slice to the gen function, and then
+// send that back to the channel? or do we send the individual
+// values for each element in the slice to the channel, and
+// then dump it from the channel into the slice?
+//
+// kk first it's not a slice it's an array and second we send the whole array
+// back but don't have to send it there to begin with
